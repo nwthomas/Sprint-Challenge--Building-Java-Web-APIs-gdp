7b13add5c4c5eeef685a1edffa0935723853c81d
Nathan Thomas <nwthomas@me.com>
2019-06-09 13:23:16 -0700

    Add all initial new server files

---
+++ gdp/src/main/java/com/lambdaschool/gdp/GdpApplication.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.gdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdpApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(GdpApplication.class, args);
+	}
+
+}
