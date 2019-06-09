675fda02dafda8affc4bffa4cacc3c81d5ed61a9
Nathan Thomas <nwthomas@me.com>
2019-06-09 15:08:56 -0700

    Make /economy and /country/{id} including exception handling

--- gdp/src/main/java/com/lambdaschool/gdp/Controller/GdpController.java
+++ gdp/src/main/java/com/lambdaschool/gdp/Controller/GdpController.java
@@ -2,6 +2,7 @@ package com.lambdaschool.gdp.Controller;
 
 import com.lambdaschool.gdp.GdpApplication;
 import com.lambdaschool.gdp.Model.GDP;
+import com.lambdaschool.gdp.exception.ResourceNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,12 +35,28 @@ public class GdpController
 		return new ResponseEntity<>(tempList, HttpStatus.OK);
 	}
 
-	@GetMapping(value = "economy", produces = {"application/json"})
+	@GetMapping(value = "/economy", produces = {"application/json"})
 	public ResponseEntity<?> getAllCountriesByGdp()
 	{
 		ArrayList<GDP> tempList = GdpApplication.ourGdpList.gdpList;
 		tempList.sort((c1, c2) -> (Integer.parseInt(c2.getGdp()) - Integer.parseInt(c1.getGdp())));
 		logger.info("/api/gdp/economy accessed at " + new Date());
 		return new ResponseEntity<>(tempList, HttpStatus.OK);
+	}
+
+	@GetMapping(value = "/country/{id}", produces = {"application/json"})
+	public ResponseEntity<?> getCountryById(@PathVariable int id)
+	{
+		GDP rtnCountry = GdpApplication.ourGdpList.findCountry(c -> c.getId() == id);
+		if (rtnCountry == null)
+		{
+			logger.info("No country with id " + id + " could be found.");
+			throw new ResourceNotFoundException("No country with id " + id + " could be found.");
+		}
+		else
+		{
+			logger.trace("/api/gdp/country/{id} accessed with id: " + id + " at " + new Date());
+			return new ResponseEntity<>(rtnCountry, HttpStatus.OK);
+		}
 	}
 }
